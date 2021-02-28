b9f9abcf3ead1296b59ff3007f8c447401aaa765
wxl <2718409379@qq.com>
2021-02-28 15:06:15 +0800

    add 新增加

--- src/com/wxl/demo/Hello.java
+++ src/com/wxl/demo/Hello.java
@@ -1,7 +1,15 @@
 package com.wxl.demo;
 
+/**
+ * hello world 增加注释
+ */
 public class Hello {
     public static void main(String[] args) {
+        System.out.println("Hello , Java !");
+        System.out.println("Hello , Java !");
+        System.out.println("Hello , Java !");
+        System.out.println("Hello , Java !");
+        System.out.println("Hello , Java !");
         System.out.println("Hello , Java !");
     }
 }
